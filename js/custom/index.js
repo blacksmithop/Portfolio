8593fedc71a63660b8df526ec28e80dde4b663b7
Your Name <you@example.com>
2022-04-08 10:09:29 +0530

    Css cleanup, Card layout

--- js/custom/index.js
+++ js/custom/index.js
@@ -14,8 +14,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     }
             );
 
+        //navbar
         $(".nav-link").toggleClass("text-white");
+        //dropdown button
         $(".fa-circle-chevron-down").toggleClass("text-white");
+        //card
+        $(".card").toggleClass("bg-dark bg-light");
+        $(".card").toggleClass("text-white text-dark");
+
     });
 
 
