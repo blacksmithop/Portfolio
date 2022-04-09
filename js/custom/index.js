780f6a773fd15d3c0f657d15ad7338d1ac9011ce
Your Name <you@example.com>
2022-04-09 11:47:11 +0530

    default to dark mode, accordion styling

--- js/custom/index.js
+++ js/custom/index.js
@@ -1,32 +1,35 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    $(".darkmode").click(function () {
-        $("body").toggleClass("dark")
-            .css(
-                $("body").hasClass("dark") ?
-                    {
-                        background: "#202225", color: "#f9f9f9"
-                    }
-                    :
-                    {
-                        background: "#f9f9f9", color: "#202225"
-                    }
-            );
-
-        //navbar
-        $(".nav-link").toggleClass("text-white");
-        //dropdown button
-        $(".fa-circle-chevron-down").toggleClass("text-white");
-
-        //card
-        $(".card").toggleClass("bg-dark bg-light");
-        $(".card").toggleClass("text-dark text-white");
-
-        //buttons
-        $(".btn").toggleClass("btn-dark btn-light");
-        $(".btn a").toggleClass("text-white text-dark");
-    });
+    toggleDarkMode();
+    $(".darkmode").click(toggleDarkMode);
 
 
 });
+
+toggleDarkMode = function () {
+    $("body").toggleClass("dark")
+        .css(
+            $("body").hasClass("dark") ?
+                {
+                    background: "#202225", color: "#f9f9f9"
+                }
+                :
+                {
+                    background: "#f9f9f9", color: "#202225"
+                }
+        );
+
+    //navbar
+    $(".nav-link").toggleClass("text-white");
+    //dropdown button
+    $(".fa-circle-chevron-down").toggleClass("text-white");
+
+    //card
+    $(".card").toggleClass("bg-dark bg-light");
+    $(".card").toggleClass("text-dark text-white");
+
+    //buttons
+    $(".btn").toggleClass("btn-dark btn-light");
+    $(".btn a").toggleClass("text-white text-dark");
+}
