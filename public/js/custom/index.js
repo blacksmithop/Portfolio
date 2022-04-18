222c8c25f05d3cb76b29c1abd90ae7704c8543d6
blacksmithop <angstycoder101@gmail.com>
2022-04-18 20:53:23 +0530

    pointers, anchor attributes, ogp image

--- public/js/custom/index.js
+++ public/js/custom/index.js
@@ -1,5 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     const toggleBtn = document.querySelector(".darkmode");
     toggleBtn.addEventListener("click", toggleDarkMode);
 
@@ -10,83 +9,83 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //enable tooltips
-enablTooltips = function () {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-}
-//enable toasts
-enableToasts = function () {
-    var toastTrigger = document.getElementById('resumeToastBtn')
-    var toastLiveExample = document.getElementById('resumeToast')
-    if (toastTrigger) {
-        toastTrigger.addEventListener('click', function () {
-            var toast = new bootstrap.Toast(toastLiveExample)
-
-            toast.show()
+enablTooltips = function() {
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        tooltipTriggerList.map(function(tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
         })
     }
-}
-// dark mode
-toggleDarkMode = function () {
-    let displayMode = document.querySelector(".darkmode").children;
-    toggleClass(["visible", "visually-hidden"], displayMode[0])
-    toggleClass(["visually-hidden", "visible"], displayMode[1])
-    // darkmode
-    toggleClass(['light', 'dark'], document.body);
-    //card
-    document.querySelectorAll('.card').forEach((e) => {
-        toggleBgColor(e);
-        toggleTextColor(e);
-    });
-    // buttons
-    document.querySelectorAll(".card-footer> .btn").forEach((e) => {
-        toggleClass(['btn-light', 'btn-dark'], e);
-        toggleTextColor(e);
-    });
-    //anchors
-    document.querySelectorAll(".btn a").forEach((e) => {
-        toggleTextColor(e);
-    });
-    //about me
-    toggleTextColor(document.querySelector(".about-me"));
-    //accordion
-    document.querySelectorAll(".accordion button").forEach((e) => {
-        toggleBgColor(e);
-        toggleTextColor(e);
-    });
-    document.querySelectorAll(".accordion-body").forEach((e) => {
-        toggleClass(['border-dark', 'border-warning'], e);
-    });
-    toggleClass(['border-dark', 'border-warning'], document.querySelector(".list-group"));
-}
-// class toggle functions for darkmode
-toggleClass = function (classList, element) {
+    //enable toasts
+enableToasts = function() {
+        var toastTrigger = document.getElementById('resumeToastBtn')
+        var toastLiveExample = document.getElementById('resumeToast')
+        if (toastTrigger) {
+            toastTrigger.addEventListener('click', function() {
+                var toast = new bootstrap.Toast(toastLiveExample)
+
+                toast.show()
+            })
+        }
+    }
+    // dark mode
+toggleDarkMode = function() {
+        let displayMode = document.querySelector(".darkmode").children;
+        toggleClass(["visible", "visually-hidden"], displayMode[0])
+        toggleClass(["visually-hidden", "visible"], displayMode[1])
+            // darkmode
+        toggleClass(['light', 'dark'], document.body);
+        //card
+        document.querySelectorAll('.card').forEach((e) => {
+            toggleBgColor(e);
+            toggleTextColor(e);
+        });
+        // buttons
+        document.querySelectorAll(".card-footer> .btn").forEach((e) => {
+            toggleClass(['btn-light', 'btn-dark'], e);
+            toggleTextColor(e);
+        });
+        //anchors
+        document.querySelectorAll(".btn a").forEach((e) => {
+            toggleTextColor(e);
+        });
+        //about me
+        toggleTextColor(document.querySelector(".about-me"));
+        //accordion
+        document.querySelectorAll(".accordion button").forEach((e) => {
+            toggleBgColor(e);
+            toggleTextColor(e);
+        });
+        document.querySelectorAll(".accordion-body").forEach((e) => {
+            toggleClass(['border-dark', 'border-warning'], e);
+        });
+        toggleClass(['border-dark', 'border-warning'], document.querySelector(".list-group"));
+    }
+    // class toggle functions for darkmode
+toggleClass = function(classList, element) {
     classList.map(v => element.classList.toggle(v))
 }
-toggleTextColor = function (element) {
+toggleTextColor = function(element) {
     ["text-dark", "text-light"].map(v => element.classList.toggle(v))
 }
-toggleBgColor = function (element) {
-    ["bg-light", "bg-dark"].map(v => element.classList.toggle(v))
-}
-//close card
-closeCard = function (e) {
+toggleBgColor = function(element) {
+        ["bg-light", "bg-dark"].map(v => element.classList.toggle(v))
+    }
+    //close card
+closeCard = function(e) {
     var el = e.target.parentElement.parentElement.parentElement.parentElement;
     el.remove();
 }
 
 //project card creator
-populateCards = function () {
+populateCards = function() {
     _card = document.querySelector(".col");
     let root = document.querySelector(".accordion-card");
     _card.remove();
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
-            myObj.forEach(function (obj) {
+            myObj.forEach(function(obj) {
                 createCard(root, _card, obj);
             });
         }
@@ -96,7 +95,7 @@ populateCards = function () {
 
 }
 
-createCard = function (root, card, obj) {
+createCard = function(root, card, obj) {
     var card = _card.cloneNode(true);
 
     let category = card.querySelector(".card-header i");
